41a283bfa287fb2398cae64a0a36e25a608ce0ec
Shritesh Jamulkar <shritesh.sj@gmail.com>
2018-05-15 03:48:11 +0530

    Added Push-Notifications

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import Expo from 'expo';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import Expo, { Notifications } from 'expo';
+import { StyleSheet, Text, View, Platform, Alert } from 'react-native';
 import store from './store';
 import { YellowBox } from 'react-native';
 import { Provider } from 'react-redux';
 import AppWithNavigationState from "./AppNavigator";
+import registerForNotifications from './services/push_notifications';
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 
 class App extends React.Component{
+    componentDidMount() {
+       registerForNotifications();
+       Notifications.addListener((notification) => {
+           const { data: { text }, origin } = notification;
+           if(origin === 'recieved' && text ) {
+               Alert.alert(
+                   'New Push Notification',
+                   text,
+                   [{text: 'OK.'}]
+               );
+           }
+       });
+    }
+
     render() {
         return(
             <Provider store={store}>
